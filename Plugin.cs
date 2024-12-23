4e92a7c12c08bd017c3fd5e360a7c7c9b4d08a7d
BlqzingIce <93460213+BlqzingIce@users.noreply.github.com>
2024-12-23 16:29:15 -0600

    Initial commit pt. 2

---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using SiraUtil.Zenject;
+using IPA;
+using IPALogger = IPA.Logging.Logger;
+
+namespace BlqzTweaks
+{
+    [Plugin(RuntimeOptions.SingleStartInit)]
+    public class Plugin
+    {
+        private IPALogger _log;
+        private Zenjector _zenjector;
+
+        [Init]
+        public void Init(IPALogger logger, Zenjector zenjector)
+        {
+            _log = logger;
+            _zenjector = zenjector;
+
+            zenjector.UseMetadataBinder<Plugin>();
+            zenjector.UseLogger(logger);
+        }
+
+        [OnStart]
+        public void OnApplicationStart()
+        {
+            
+        }
+
+        [OnExit]
+        public void OnApplicationQuit()
+        {
+            
+        }
+    }
+}
